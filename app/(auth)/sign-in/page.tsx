4ffbcdd0c34cd12dc721db9f17c53b13e2106ea9
Ayush <ayushp1602@gmail.com>
2026-01-30 05:46:15 -0600

    implement auth logic

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,9 +4,15 @@ import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import InputField from '@/components/forms/InputField';
 import FooterLink from '@/components/forms/FooterLink';
+import {signInWithEmail} from "@/lib/actions/auth.actions";
+import {router} from "next/client";
+import {useRouter} from "next/navigation";
+import {toast} from "sonner";
 
 
 const SignIn = () => {
+
+    const router = useRouter();
 
     const {
         register,
@@ -22,9 +28,13 @@ const SignIn = () => {
 
     const onSubmit = async (data: SignInFormData) => {
         try {
-            console.log(data);
+            const result = await signInWithEmail(data);
+            if(result.success) router.push('/');
         } catch (e) {
             console.error(e);
+            toast.error('Sign in failed', {
+                description: e instanceof Error ? e.message : 'Failed to sign in.'
+            })
         }
     }
 
